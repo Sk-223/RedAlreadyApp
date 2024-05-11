fde7869d409fe09c277a8aa79a6c53f6b4673535
Sharon Kaufman <kaufman0106@gmail.com>
2024-05-10 21:02:17 -0400

    Create Sidebar Subcategories

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-// App.js
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store'; 
 import Header from './components/Header/Header';
 import MainSection from './components/MainSection/MainSection';
+import Sidebar from './components/Sidebar/Sidebar';
+import styles from './App.module.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <Header />
-      <MainSection />
+      <div className={styles.appContainer}>
+        <Header />
+        <div className={styles.contentContainer}>
+          <MainSection />
+          <Sidebar />
+        </div>
+      </div>
     </Provider>
   );
 }
