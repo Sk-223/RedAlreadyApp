141c284469e130c885e3ab9f3826e75bf7f26411
Sharon Kaufman <kaufman0106@gmail.com>
2024-05-15 19:38:36 -0400

    Add Detailed View for Cards

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 import SubredditContent from './components/SubredditContent/SubredditContent';
 import Header from './components/Header/Header';
 import MainSection from './components/MainSection/MainSection';
+import PostDetailView from './components/PostDetailView/PostDetailView';
 import Sidebar from './components/Sidebar/Sidebar';
 import styles from './App.module.css';
 
@@ -15,9 +16,12 @@ function App() {
         <Header />
         <div className={styles.contentContainer}>
           <Routes>
-            {/* A Route for each subreddit and a default route */}
-            <Route index element={<MainSection />} />    
-            <Route path="/r/:subreddit" element={<SubredditContent />} />
+           {/* A Route for each subreddit and a default route */}
+           <Route index element={<MainSection />} />
+            {/* Route for SubredditContent  */}
+            <Route path="/r/:subreddit" element={<SubredditContent />} /> 
+            {/* Route for PostDetailView */}
+            <Route path="/r/:subreddit/:postId" element={<PostDetailView />} />
           </Routes>
           <Sidebar className={styles.sidebar}/>
         </div>
---
+++ src/components/PostDetailView/PostDetailView.js
@@ -0,0 +1,48 @@
+// PostDetailView.js
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import PostCard from "../PostCard/PostCard";
+import CommentSection from "../CommentSection/CommentSection"; // Import CommentSection
+import styles from "./PostDetailView.module.css";
+import { fetchComments } from "../../slices/postsSlice";
+
+function PostDetailView() {
+  const { postId, subreddit } = useParams(); // get the post and subreddit name from the url
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { postsBySubreddit, isLoading, error } = useSelector(
+    (state) => state.posts
+  );
+  const post = postsBySubreddit[subreddit]?.find((p) => p.id === postId);
+
+  useEffect(() => {
+    if (!post) {
+      navigate(`/r/${subreddit}`);
+    } else {
+      dispatch(fetchComments(postId));
+    }
+  }, [subreddit, post, navigate, dispatch]);
+
+  if (isLoading) {
+    return <p>Loading posts...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  if (!post) {
+    return <p>Post not found.</p>;
+  }
+
+  return (
+    <div className={styles.postDetailView}>
+      <PostCard post={post} />
+      <CommentSection postId={postId} subreddit={subreddit} />
+    </div>
+  );
+}
+
+export default PostDetailView;
